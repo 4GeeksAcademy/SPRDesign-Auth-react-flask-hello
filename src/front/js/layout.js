7d99e8774b846773d0cfdf6faffdb1be498fef10
Sara Pages <144797101+SPRDesign@users.noreply.github.com>
2024-02-21 00:39:32 +0000

    first

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,6 +11,11 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
+import { Login } from "./pages/login";
+import { SignUp } from "./pages/signup";
+import { Private } from "./pages/private";
+
+
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -26,6 +31,9 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Private />} path="/private" />
+                        <Route element={<SignUp />} path="/signup" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
